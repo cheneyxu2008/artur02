3200430292e1f4545927180b6306096312ea54de
artur.herczeg <artur.herczeg@85e5d6a6-f088-11dd-a70c-37889e090ee0>
2010-11-22 20:38:42 +0000

    C++ WaitableTimer example

---
+++ trunk/CPP/WaitableTimer/WaitableTimer.cpp
@@ -0,0 +1,48 @@
+// WaitableTimer.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int count = 0;	// Iteration counter
+
+	HANDLE hTimer = NULL;	// WaitableTimer handle
+	hTimer = CreateWaitableTimer(	// Create waitable timer
+		NULL,
+		FALSE,	// Autoreset --> timer restarts counting after event fired
+		NULL);
+
+	SYSTEMTIME time;		// System time structure
+	GetSystemTime(&time);	// Curren time
+	time.wSecond += 3;		// Wait 3 sec
+
+	FILETIME ftime;	// File time (expected by SetWaitableTimer)
+	SystemTimeToFileTime(&time, &ftime);	// Convert system to file time
+
+	if(!SetWaitableTimer(	// Set waitable timer
+		hTimer,	// Timer handle
+		reinterpret_cast<LARGE_INTEGER*>(&ftime),	// Convert file time to large integer
+		2000,	// Period time, fire again after 2 sec
+		NULL,
+		NULL,
+		0))
+	{
+			cout << "SetWaitableTimer failed" <<  GetLastError() << endl;
+	};
+
+	
+	while(WaitForSingleObject(hTimer, 5000) == WAIT_OBJECT_0){	// Wait for timer event
+		cout << "CALLED " << ++count << endl;
+		if(count+1 > 5){	// Exit after 5 events
+			break;
+		}
+	}
+		
+	CancelWaitableTimer(hTimer);	// Stop timer
+	CloseHandle(hTimer);			// Delete handle
+
+	return 0;
+}
+
