6b6aa4c2699287cea9fbbe19977dfaa05bc8250f
artur.herczeg@gmail.com <artur.herczeg@gmail.com@85e5d6a6-f088-11dd-a70c-37889e090ee0>
2011-10-27 12:57:56 +0000

    Class inspector project

---
+++ trunk/ClassInspector/ClassInspector/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Cci;
+
+namespace ClassInspector
+{
+    public static class Inspector
+    {
+        public static IEnumerable<IPropertyDefinition> GetProperties(INamedTypeDefinition type)
+        {
+            return type.Properties;
+        }
+
+        public static IEnumerable<string> GetPropertyNames(INamedTypeDefinition type)
+        {
+            var properties = GetProperties(type);
+            return properties.Select(GetPropertyName);
+        }
+
+        public static IEnumerable<ITypeReference> GetBaseClasses(INamedTypeDefinition type)
+        {
+            return type.BaseClasses;
+        }
+
+        public static IEnumerable<string> GetBaseClassNames(INamedTypeDefinition type)
+        {
+            return GetBaseClasses(type).Select(baseClass => baseClass.ResolvedType.ToString());
+        }
+
+        public static IEnumerable<ITypeReference> GetInterfaces(INamedTypeDefinition type)
+        {
+            return type.Interfaces;
+        }
+
+        public static IEnumerable<ITypeReference> GetGenericArguments(ITypeReference implementedInterface)
+        {
+            return implementedInterface.ResolvedType.InstanceType.GenericArguments;
+        }
+
+        public static string GetPropertyName(IPropertyDefinition property)
+        {
+            return property.Name.Value.ToString();
+        }
+    }
+}
