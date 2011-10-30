67fe50e0b053356cf5d520d6316083d17b930371
artur.herczeg@gmail.com <artur.herczeg@gmail.com@85e5d6a6-f088-11dd-a70c-37889e090ee0>
2011-10-30 12:08:56 +0000

    Class inspector refactored

---
+++ ClassInspector/ClassInspector/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace ClassInspector
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Merges two dictionaries
+        /// </summary>
+        /// <param name="to">The values are merged into</param>
+        /// <param name="from">The source of the values</param>
+        public static void Merge(this IDictionary to, IDictionary from)
+        {
+            foreach (var key in from.Keys)
+            {
+                to.Add(key, from[key]);
+            }
+        }
+    }
+}
---
+++ ClassInspector/ClassInspector/Initializer.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+
+namespace TypeInspector
+{
+    /// <summary>
+    /// Initializes types to default values
+    /// </summary>
+    public class Initializer
+    {
+        private bool _typeBool = true;
+        private int _stringCounter;
+
+        /// <summary>
+        /// Initializes type
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public string Initialize(string type)
+        {
+            switch(type)
+            {
+                case "System.String":
+                    return GetString();
+                case "System.Boolean":
+                    return GetBool().ToString().ToLowerInvariant();
+                default:
+                    return string.Empty;
+
+            }
+        }
+
+        /// <summary>
+        /// Get Booleand default value
+        /// </summary>
+        /// <param name="invert"></param>
+        /// <returns></returns>
+        public bool GetBool(bool invert = false)
+        {
+            var result = _typeBool;
+            if(invert)
+            {
+                _typeBool = !_typeBool;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Get string default value
+        /// </summary>
+        /// <returns></returns>
+        public string GetString()
+        {
+            return string.Format(CultureInfo.InvariantCulture, "\"string{0}\"", _stringCounter++);
+        }
+    }
+}
