ad1851ed727af6cb24bc0e8b609c4c13944ae3e4
artur.herczeg@gmail.com <artur.herczeg@gmail.com@85e5d6a6-f088-11dd-a70c-37889e090ee0>
2011-11-02 06:27:27 +0000

    WPF client

---
+++ ClassInspector/ClassInspectorWpf/ViewModels/AssemblyInspectedVM.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using ClassInspectorWpf.Models;
+using Microsoft.Cci;
+
+namespace ClassInspectorWpf.ViewModels
+{
+    public class AssemblyInspectedVM : INotifyPropertyChanged
+    {
+        private AssemblyInspected _assemblyInspected;
+
+        public AssemblyInspected AssemblyInspected
+        {
+            get { return _assemblyInspected; }
+            set
+            {
+                _assemblyInspected = value;
+
+                using (var host = new PeReader.DefaultHost())
+                {
+                    var assembly =
+                        host.LoadUnitFrom(
+                            _assemblyInspected.Path)
+                        as IAssembly;
+
+                    if (assembly != null)
+                    {
+                        _assemblyInspected.AllTypes = assembly.GetAllTypes();
+                    }
+                }
+            }
+        }
+
+        public List<string> TypeNames { 
+            get { return new List<string>()
+                             {
+                                 "aaa",
+                                 "bbb"
+                             }; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
