5d8002ccdb760cefa3ab88feb57a4ced05b06a25
artur.herczeg@gmail.com <artur.herczeg@gmail.com@85e5d6a6-f088-11dd-a70c-37889e090ee0>
2011-11-02 06:27:27 +0000

    WPF client

---
+++ trunk/ClassInspector/ClassInspectorWpf/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClassInspectorWpf.Models;
+using ClassInspectorWpf.ViewModels;
+using Microsoft.Win32;
+
+namespace ClassInspectorWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AssemblyInspectedVM _assemblyVM = new AssemblyInspectedVM();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openAssemblyDialog = new OpenFileDialog()
+                                                    {
+                                                        CheckFileExists = true,
+                                                        Filter = "All files|*.*|DLLs|*.dll|Executables|*.exe"
+                                                    };
+            if (openAssemblyDialog.ShowDialog(this).GetValueOrDefault())
+            {
+                var _assemblyInspected = new AssemblyInspected();
+                _assemblyInspected.Path = openAssemblyDialog.FileName;
+                _assemblyVM.AssemblyInspected = _assemblyInspected;
+
+                listBox1.ItemsSource = _assemblyVM.TypeNames;
+            }
+        }
+    }
+}
---
+++ trunk/ClassInspector/ClassInspectorWpf/Models/AssemblyInspected.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Microsoft.Cci;
+
+namespace ClassInspectorWpf.Models
+{
+    public class AssemblyInspected
+    {
+        public string Path { get; set; }
+        public IEnumerable<INamedTypeDefinition> AllTypes { get; set; }
+    }
+}
