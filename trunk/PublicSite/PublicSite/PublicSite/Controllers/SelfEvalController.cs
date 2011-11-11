0641418e33961a5bd0f818e0125e290fed41bdb7
artur.herczeg@gmail.com <artur.herczeg@gmail.com@85e5d6a6-f088-11dd-a70c-37889e090ee0>
2011-11-11 16:16:51 +0000

    Public ASP.NET MVC site initial checkin

---
+++ trunk/PublicSite/PublicSite/PublicSite/Controllers/SelfEvalController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PublicSite.Controllers
+{
+    public class SelfEvalController : Controller
+    {
+        //
+        // GET: /SelfEval/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
