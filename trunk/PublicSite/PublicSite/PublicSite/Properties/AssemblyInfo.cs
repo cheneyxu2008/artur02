a3c75dc650f1a526a6a544c61f03958b4d5ad604
artur.herczeg@gmail.com <artur.herczeg@gmail.com@85e5d6a6-f088-11dd-a70c-37889e090ee0>
2011-11-14 12:19:53 +0000

    LESS CSS configuration

--- trunk/PublicSite/PublicSite/PublicSite/Properties/AssemblyInfo.cs
+++ trunk/PublicSite/PublicSite/PublicSite/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PublicSite")]
+[assembly: AssemblyTitle("Public Website")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("PublicSite")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyProduct("Artur Herczeg's Public Website")]
+[assembly: AssemblyCopyright("Copyright © Artur Herczeg")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,3 +34,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
