7216ed72c6d1fc3b6620f8837da96c0915e51cc5
artur.herczeg@gmail.com <artur.herczeg@gmail.com@85e5d6a6-f088-11dd-a70c-37889e090ee0>
2012-06-25 10:04:19 +0000

    RoslynExamples

---
+++ Roslyn/RoslynExamples/SyntaxTransformation/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Roslyn.Compilers;
+using Roslyn.Compilers.CSharp;
+using Roslyn.Services;
+using Roslyn.Services.CSharp;
+
+namespace SyntaxTransformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            SyntaxTree tree = SyntaxTree.ParseCompilationUnit(
+                @"using System;
+                using System.Collections;
+                using System.Linq;
+                using System.Text;
+ 
+                namespace HelloWorld
+                {
+                    class Program
+                    {
+                        static void Main(string[] args)
+                        {
+                            Console.WriteLine(""Hello, World!"");
+                        }
+                    }
+                }");
+
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+
+            NameSyntax name = Syntax.IdentifierName("System");
+            name = Syntax.QualifiedName(name, Syntax.IdentifierName("Collections"));
+            name = Syntax.QualifiedName(name, Syntax.IdentifierName("Generic"));
+
+            var oldUsing = root.Usings[1];
+            var newUsing = oldUsing.WithName(name);
+            root = root.ReplaceNode(oldUsing, newUsing);
+
+        }
+    }
+}
